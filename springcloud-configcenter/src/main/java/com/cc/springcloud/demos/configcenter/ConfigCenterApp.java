d58486aaf9c1fc82c4ac03378a32dca95578eaa8
sen.hu <sen.hu@zhaopin.com.cn>
2018-10-17 17:23:35 +0800

    测试zookeeper配置中心

--- springcloud-configcenter/src/main/java/com/cc/springcloud/demos/configcenter/ConfigCenterApp.java
+++ springcloud-configcenter/src/main/java/com/cc/springcloud/demos/configcenter/ConfigCenterApp.java
@@ -3,12 +3,14 @@ package com.cc.springcloud.demos.configcenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.cc.springcloud.demos.configcenter"})
 public class ConfigCenterApp
 {
     public static void main( String[] args )
--- springcloud-configcenter/src/main/java/com/cc/springcloud/demos/configcenter/TestConfig.java
+++ springcloud-configcenter/src/main/java/com/cc/springcloud/demos/configcenter/TestConfig.java
@@ -1,12 +1,14 @@
 package com.cc.springcloud.demos.configcenter;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 public class TestConfig {
 
-    @Value("${nodelist}")
+    @Value("${redislist}")
     private String redisNodeConfig;
 
     public String getRedisNodeConfig() {
